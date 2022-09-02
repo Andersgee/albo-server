b7b05f5764ebeb283620c1b1b024e802fe7c1142
andy <andersgee@gmail.com>
2022-09-02 22:18:28 +0200

    seems good

--- src/lib.rs
+++ src/lib.rs
@@ -1,79 +1,53 @@
-use js_sys::Uint8Array;
-use wasm_bindgen::prelude::*;
+//use js_sys::Uint8Array;
+//use wasm_bindgen::prelude::*;
 
 //tutorial writing a tiny ECS in rust
 //https://ianjk.com/ecs-in-rust/
 
-#[wasm_bindgen]
-extern "C" {
-  #[wasm_bindgen(js_namespace = console)]
-  fn log(s: &str);
+struct Health(i32);
+struct Name(&'static str);
 
-  #[wasm_bindgen(js_namespace = Math)]
-  fn random() -> f64;
+struct World {
+  health_components: Vec<Option<Health>>,
+  name_components: Vec<Option<Name>>,
 }
-
-enum Vao {
-  floor,
-  bird,
-}
-
-pub struct Renderable {
-  vao: Vao,
-  model_mat: [f32; 16],
-}
-
-pub struct Movable {
-  x: f32,
-  y: f32,
-}
-
-enum Entity {
-  Renderable(Option<Renderable>),
-  Movable(Movable),
-}
-
-#[wasm_bindgen]
-pub struct World {
-  entities: Vec<Entity>,
-}
-
-#[wasm_bindgen]
 impl World {
-  #[wasm_bindgen(constructor)]
-  pub fn new() -> World {
-    log("instantiating new Example");
-
-    //webgl::start().unwrap();
-
-    let b = Movable { x: 1.2, y: 3.5 };
-
-    //let home = Entity(Movable { x: 1.1, y: 2.2 });
-
-    //let c =  Entity::Movable { {x: 1.2, y: 3.5} };
-
-    World {
-      entities: vec![{ b }],
+  fn new() -> Self {
+    Self {
+      health_components: Vec::new(),
+      name_components: Vec::new(),
     }
   }
 
-  #[wasm_bindgen(getter)]
-  pub fn state(&self) -> f64 {
-    3.1
+  fn new_entity(&mut self, health: Option<Health>, name: Option<Name>) {
+    self.health_components.push(health);
+    self.name_components.push(name);
   }
 }
 
-pub fn add(left: usize, right: usize) -> usize {
-  left + right
-}
+fn hej() {
+  let mut world = World::new();
+  // Icarus's health is *not* looking good.
+  world.new_entity(Some(Health(-10)), Some(Name("Icarus")));
+  // Prometheus is very healthy.
+  world.new_entity(Some(Health(100)), Some(Name("Prometheus")));
+  // Note that Zeus does not have a `Health` component.
+  world.new_entity(None, Some(Name("Zeus")));
 
-#[cfg(test)]
-mod tests {
-  use super::*;
+  let zip = world
+    .health_components
+    .iter()
+    .zip(world.name_components.iter());
 
-  #[test]
-  fn it_works() {
-    let result = add(2, 2);
-    assert_eq!(result, 4);
+  let with_health_and_name = zip.filter_map(|(health, name): (&Option<Health>, &Option<Name>)| {
+    Some((health.as_ref()?, name.as_ref()?))
+  });
+
+  for (health, name) in with_health_and_name {
+    if health.0 < 0 {
+      println!("{} has perished!", name.0);
+    } else {
+      println!("{} is still healthy", name.0);
+    }
   }
 }
