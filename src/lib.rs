3b848f7eb9c8d749c89d224b139e0863a170a4ee
andy <andersgee@gmail.com>
2022-09-02 22:08:05 +0200

    found a tutorial, lets follow along

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 use js_sys::Uint8Array;
 use wasm_bindgen::prelude::*;
 
+//tutorial writing a tiny ECS in rust
+//https://ianjk.com/ecs-in-rust/
+
 #[wasm_bindgen]
 extern "C" {
   #[wasm_bindgen(js_namespace = console)]
@@ -27,7 +30,7 @@ pub struct Movable {
 
 enum Entity {
   Renderable(Option<Renderable>),
-  Movable(Option<Movable>),
+  Movable(Movable),
 }
 
 #[wasm_bindgen]
@@ -43,7 +46,15 @@ impl World {
 
     //webgl::start().unwrap();
 
-    World { entities: vec![] }
+    let b = Movable { x: 1.2, y: 3.5 };
+
+    //let home = Entity(Movable { x: 1.1, y: 2.2 });
+
+    //let c =  Entity::Movable { {x: 1.2, y: 3.5} };
+
+    World {
+      entities: vec![{ b }],
+    }
   }
 
   #[wasm_bindgen(getter)]
