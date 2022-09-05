08c305a83445d0c6aa637eca6f5f23ababb00761
andy <andersgee@gmail.com>
2022-09-06 01:22:59 +0200

    add_inplace

--- src/systems/transform.rs
+++ src/systems/transform.rs
@@ -1,18 +1,18 @@
+use std::borrow::Borrow;
+
 use crate::{components, resources};
-use gl_matrix::vec3;
+use gl_matrix::{common::Vec3, vec3};
 use legion::*;
+
+pub fn add_inplace(out: &mut Vec3, v: &Vec3) -> Vec3 {
+  out[0] = out[0] + v[0];
+  out[1] = out[1] + v[1];
+  out[2] = out[2] + v[2];
+
+  *out
+}
 
 #[system(for_each)]
 pub fn transform(transform: &mut components::Transform, #[resource] time: &resources::Time) {
-  /*
-   vec3::add(
-     &mut transform.position.to_owned(),
-     &transform.position,
-     &transform.velocity,
-   );
-  */
-
-  transform.position[0] = transform.position[0] + transform.velocity[0];
-  transform.position[1] = transform.position[1] + transform.velocity[1];
-  transform.position[2] = transform.position[2] + transform.velocity[2];
+  add_inplace(&mut transform.position, &transform.velocity);
 }
