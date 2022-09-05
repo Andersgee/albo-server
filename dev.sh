97510662f0b7fc6534f2c98fbc238fbc3816df65
andy <andersgee@gmail.com>
2022-09-06 01:59:17 +0200

    added wasm-opt to dev.sh

--- dev.sh
+++ dev.sh
@@ -1,2 +1,13 @@
+#!/bin/bash
+source "/home/andy/.bash_aliases"
+shopt -s expand_aliases
+
+#build (uses default LLVM optimization whic is speed)
 wasm-pack build --target web
+
+#optimize with wasm-opt (Binaryen toolkit) which goes further than LLVM's WebAssembly backend
+# -O2 might be the better option? see wasm-opt --help
+wasm-opt pkg/nextjs_rust_playground_server_bg.wasm -O4 -o pkg/optimized.wasm
+
+#start server
 deno run --allow-net --allow-read main.ts
